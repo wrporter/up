43cdc0c0f18481b988d63462c7b8a601a287ee8f
Wesley Porter <wesp@qualtrics.com>
2022-07-08 10:04:19 -0600

    Add OAuth secrets to github workflow

--- apps/checkit-app/.ci/inject-docker-variables.sh
+++ apps/checkit-app/.ci/inject-docker-variables.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "-- ${0} start..."
 
-sed -i '' "s/\${GOOGLE_OAUTH_CLIENT_ID}/${GOOGLE_OAUTH_CLIENT_ID}/g" .ci/docker-compose.tmp.yml
-sed -i '' "s/\${GOOGLE_OAUTH_CLIENT_SECRET}/${GOOGLE_OAUTH_CLIENT_SECRET}/g" .ci/docker-compose.tmp.yml
+sed -i '' "s/\${GOOGLE_OAUTH_CLIENT_ID}/${CHECKIT_GOOGLE_OAUTH_CLIENT_ID}/g" .ci/docker-compose.tmp.yml
+sed -i '' "s/\${GOOGLE_OAUTH_CLIENT_SECRET}/${CHECKIT_GOOGLE_OAUTH_CLIENT_SECRET}/g" .ci/docker-compose.tmp.yml
 
 echo "-- ${0} complete!"
